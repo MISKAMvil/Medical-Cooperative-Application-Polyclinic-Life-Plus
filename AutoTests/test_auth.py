378daa2349a012bd2a68c0cf95f074652c301a64
Scarbby1 <91124162+Scarbby1@users.noreply.github.com>
2023-12-12 19:07:33 +0000

    test_auth update

--- AutoTests/test_auth.py
+++ AutoTests/test_auth.py
@@ -1,3 +1,5 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import uuid
 
@@ -10,25 +12,40 @@ def auth(driver):
 
 def login(driver):
     try:
-        driver.get("http://localhost:5000/auth/login")
-        input_username = driver.find_element(By.XPATH, "/html/body/main/div/div/form/div[1]/input")
-        input_password = driver.find_element(By.XPATH, "/html/body/main/div/div/form/div[2]/input")
+        driver.get("http://172.16.238.10:8000/auth/login")
+        input_username = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, "/html/body/main/div/div/form/div[1]/input"))
+        )
+        input_password = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, "/html/body/main/div/div/form/div[2]/input"))
+        )
+
+        # Используем случайные данные
         randomize = str(uuid.uuid4())
-        login = str(randomize)
-        password = str(randomize)
         login = 'user1'
         password = 'qwerty'
+
         data = {
             'login': login,
             'password': password,
         }
         print(data)
+
         input_username.send_keys(data['login'])
         input_password.send_keys(data['password'])
-        driver.find_element(By.XPATH, "/html/body/main/div/div/form/button").click()
+
+        button = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, "/html/body/main/div/div/form/button"))
+        )
+        
+        print("Попытка выполнить click()")
+        button.click()
+        print("click() выполнен успешно")
+
         if "Вы успешно аутентифицированы." in driver.find_element(By.XPATH, "/html/body/div").text:
             return True
         else:
             return False
-    except:
+    except Exception as e:
+        print(f"An error occurred: {e}")
         return False
